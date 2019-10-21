431711636cc71404eedbd33acec42fd96cc16220
sajid9505 <42285255+sajid9505@users.noreply.github.com>
2019-10-21 23:28:10 +0600

    Changes with progress bar

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
 /* Add your script here */
 
+
+
+  // Taking inputs
+var StartTemp= document.getElementById("StartTemp").value;
+var StopTemp= document.getElementById("StopTemp").value;
+var TimeInterval= document.getElementById("TimeInterval").value;
+
+
+
+
+// Progress bar
 var i = 0;
 function move() {
   if (i == 0) {
     i = 1;
-    var elem = document.getElementById("myBar");
-    var width = 1;
-    var id = setInterval(frame, 10);
+    var element = document.getElementById("myBar");
+    var StartTemp= document.getElementById("StartTemp").value;
+    var StopTemp= document.getElementById("StopTemp").value;
+    var TimeInterval= document.getElementById("TimeInterval").value;
+    // var width = 1;
+    var id = setInterval(frame, TimeInterval);
     function frame() {
-      if (width >= 100) {
+      if (StartTemp >= StopTemp) {
         clearInterval(id);
         i = 0;
       } else {
-        width++;
-        elem.style.width = width + "%";
+        StartTemp++;
+        element.style.width = StartTemp + '%';
+        element.innerHTML = StartTemp * 1  + '%';
       }
+    }
+      if(element.innerHTML <= 20){
+        element.style.color(blue);
     }
   }
 }
+
+// $('.Btn').click(function(){
+//   var $this = $(this).toggleClass('Btn');
+//      if($(this).hasClass('Btn'))
+//              {
+//            $(this).text('Start');
+
+//          } else {
+//            $(this).text('Cooking');
+//              }
+// });
+
